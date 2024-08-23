7928631312e3b7073ff0a3e6cecdafb6adca1b16
Le Duc Thai <leducthai1008@gmail.com>
2024-08-23 17:36:41 +0700

    Giao diện trang quản lý sản phẩm

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,6 @@
+//[GET] /admin/product
+module.exports.index = (req, res) => {
+    res.render("admin/pages/product/index", {
+        pageTitle: "Danh sách sản phẩm"
+    })
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,11 @@
 const dashboardRoute = require("./dashboard.route");
+const productRoutes = require("./product.route");
 const system = require("../../configs/system");
 
 module.exports = (app) => {
     const PATH_ADMIN = `/${system.prefixAdmin}`;
 
-    app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute)
+    app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
+
+    app.use(`${PATH_ADMIN}/products`, productRoutes);
 }

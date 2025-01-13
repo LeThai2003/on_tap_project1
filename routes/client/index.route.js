9afae553b7a4a80c195e2b01903986f48d17982a
Le Duc Thai <leducthai1008@gmail.com>
2025-01-13 14:38:21 +0700

    iot

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,11 @@
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
+const dataIotRoutes = require("./iot.route");
 
 module.exports = (app) => {
     app.use("/", homeRoutes);
     
     app.use("/products", productRoutes);
+
+    app.use("/iot", dataIotRoutes);
 }
